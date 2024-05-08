4f3d80b521b4a8b93394dfbeafa8ba44b4181c18
Edgar JC <ejuarez01@alumnos.uaq.mx>
2024-05-08 07:48:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res,next) =>{
+    console.log(req);
+    res.send("Hello Wold!");
+})
+
+app.get("/pokemon", (req, res, next) => {
+    res.json({ name: "Pikachu", type: "Electric" });
+  });
+
+app.listen(3000, () => {
+        console.log("Server is running . . .");
+    })
+    
